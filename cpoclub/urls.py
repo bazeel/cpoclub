6493d9dc2801ee3d170d00264634a91efe4947b2
bazeel <vasiliyzaycev1989@gmail.com>
2014-03-03 10:29:12 +0400

    auth pass reset

--- cpoclub/urls.py
+++ cpoclub/urls.py
@@ -10,13 +10,20 @@ urlpatterns = patterns('',
     # url(r'^$', 'cpoclub.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/', include('account.urls')),
 
-    url(r'^password-reset/$', auth_views.password_reset, name='account_password_reset'),
-    url(r'^password-reset/done/$', auth_views.password_reset_done, name='account_password_reset_done'),
-    url(r'^password-reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)$', auth_views.password_reset_confirm, 
-        name='account_password_reset_confirm'),
-    url(r'^password-reset/complete/$', auth_views.password_reset_complete, name='account_password_reset_complete'),
+
+    #url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    #url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+
+    #url(r'^password_change/$', 'django.contrib.auth.views.password_change', name='password_change'),
+    #url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
+    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
+    url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        name='password_reset_confirm'),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
+
 )
