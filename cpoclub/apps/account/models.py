8bc0e70fb5a29cb0359072940b8d75e3ddf3c94f
bazeel <vasiliyzaycev1989@gmail.com>
2014-03-04 18:37:27 +0400

    profile

--- cpoclub/apps/account/models.py
+++ cpoclub/apps/account/models.py
@@ -18,6 +18,9 @@ class InvitationCode(models.Model):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, verbose_name=_('user'), related_name='profile')
     invitation_code = models.OneToOneField(InvitationCode, verbose_name=_('invitation code'), related_name='profile')
+    avatar = models.ImageField(_('avatar'), help_text=_('avatar'), upload_to='avatar', blank=True)
+    organization = models.CharField(_('organization'), max_length=255, blank=True)
+    post = models.CharField(_('post'), max_length=255, blank=True)
 
     class Meta:
         verbose_name = _('user profile')
--- cpoclub/settings.py
+++ cpoclub/settings.py
@@ -54,22 +54,6 @@ ROOT_URLCONF = 'urls'
 
 WSGI_APPLICATION = 'wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'cpoclub',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'qwerty',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
