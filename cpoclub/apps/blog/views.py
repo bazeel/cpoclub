5cbd3c2708f8d60e484e90d67305dc8f84a60020
bazeel <vasiliyzaycev1989@gmail.com>
2014-03-21 18:29:49 +0400

    blog comment add

--- cpoclub/apps/blog/views.py
+++ cpoclub/apps/blog/views.py
@@ -1,8 +1,10 @@
 import calendar
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 from datetime import date
 from django.views.generic import ListView
-from .models import Record
+from .models import Record, Comment
 
 
 class CustomRecordList(ListView):
@@ -31,6 +33,16 @@ class CustomRecordList(ListView):
 
 def record_detail(request, pk):
     obj = get_object_or_404(Record, pk=pk)
+
+    u = request.user
+    if not obj.public and not u.is_authenticated():
+        return HttpResponseRedirect(reverse('account_login'))
+
+    if request.method == 'POST' and u.is_authenticated():
+        comment_msg = request.POST.get('comment', '')
+        if comment_msg:
+            comment_obj = Comment(user=u, record=obj, active=True, comment=comment_msg)
+            comment_obj.save()
 
     return render(request, 'blog/record_detail.html', {
         'object': obj})
